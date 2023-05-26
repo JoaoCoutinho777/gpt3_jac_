9718d8e0e605b204e11a964c4854a6d6ae825770
jac <joao.coutinho@sebasi.pt>
2023-05-26 17:37:07 +0100

    navbar built

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './features.css';
+
+const Feature = () => {
+    return (
+    <div>Article</div>
+    )
+}
+
+export default Feature;
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './possibility.css';
+
+const Posibility = () => {
+    return (
+    <div>Article</div>
+    )
+}
+
+export default Posibility;
